e570efab9287f9a01b5fbe097b06f36a63f65add
tanmaysingh27 <111690894+tanmaysingh27@users.noreply.github.com>
2023-06-21 11:53:25 +0530

    FFT Update
    Takes the audio sample from the microphone and performs FFT to the audio sample and displays in the Seriall Monitor.

--- AudioInput/AudioInput.ino
+++ AudioInput/AudioInput.ino
@@ -1,19 +1,38 @@
-int micInput = A1;
+#include<fix_fft.h>
 
+#define samples 12
+
+int micInput = A1; // Audio Input
 int baseline = 337;
+int amplitude,mic;
+// const int max_amplitude = 157;
 
-int amplitude;
-const int max_amplitude = 165;
-// const int delta_ampliude = max_amplitude/
-
+char data[128],im[128];
+char data_out[20];
 
 void setup(){
-  Serial.begin(9600);
+Serial.begin(9600);
 }
 
 void loop(){
-  mic = analogRead(micInput);
-  amplitude = abs(mic-baseline);
+//Taking input of the microphone from the anlog input
+for (int i = 0;i<128;i++){
+mic = analogRead(micInput);  // Analog Input of the audio sample
+amplitude = abs(mic-baseline); //Getitng the amplitude of the audio sample
+data[i] = amplitude;
+im[i] = 0; 
+}
 
-  Serial.println(amplitude);
+//Perform FFT to the sample
+fix_fft(data, im, 7, 0);
+
+for (int i = 0; i < 64; i++) {
+  data[i] = sqrt(data[i] * data[i] + im[i] * im[i]);
+}
+
+//prints out the FFT in char forms in serial monitor
+for (int i = 0; i < 20; i++){
+  data_out[i] = (data[i * 3] + data[i * 3 + 1] + data[i * 3 + 2])/3;
+  Serial.print(data_out[i]);
+}
 }
